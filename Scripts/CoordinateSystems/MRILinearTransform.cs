57b3e1a0aa7d4d9a12a924d4efc756c69f4feb5f
Dan Birman <danbirman@gmail.com>
2022-08-31 20:50:52 -0700

    Adding the MRI-linear transform
    Still missing the CCF rotation

--- Scripts/CoordinateSystems/MRILinearTransform.cs
+++ Scripts/CoordinateSystems/MRILinearTransform.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class MRILinearTransform : CoordinateTransform
 {
-    private Vector3 invivoConversionAPMLDV = new Vector3(1.087f, 1f, 0.952f);
+    //ML_SCALE = 0.952
+    //DV_SCALE = 0.885  # multiplicative factor on DV dimension, determined from MRI->CCF transform
+    //AP_SCALE = 1.031  # multiplicative factor on AP dimension
+
+    private Vector3 invivoConversionAPMLDV = new Vector3(-1.031f, 0.952f, -0.885f);
+    private Vector3 inverseConversion = new Vector3(-1 / 1.031f, 1f / 0.952f, -1 / 0.885f);
     private Vector3 invivoConversionPhiThetaBeta = new Vector3(0f, 7f, 0f);
     private Vector3 bregma = new Vector3(5.4f, 5.7f, 0.332f);
 
@@ -23,19 +28,21 @@ public class MRILinearTransform : CoordinateTransform
         }
     }
 
+
     /// <summary>
-    /// Convert a coordinate in Needles space back to CCF space
+    /// Convert a coordinate in MRI-Linear space back to CCF space
     /// </summary>
     /// <param name="needlesCoord">Coordinate should be relative to bregma (ap,ml,dv)</param>
     /// <returns></returns>
     /// <exception cref="System.NotImplementedException"></exception>
     public override Vector3 ToCCF(Vector3 needlesCoord)
     {
-        throw new System.NotImplementedException();
+        Vector3 ccfCoord = Vector3.Scale(needlesCoord, inverseConversion) + bregma;
+        return ccfCoord;
     }
 
     /// <summary>
-    /// Convert a coordinate in CCF space back to needles space
+    /// Convert a coordinate in CCF space back to MRI-linear space
     /// </summary>
     /// <param name="ccfCoord">Coordinate should be relative to CCF (ap=0, ml=0, dv=0)</param>
     /// <returns></returns>
@@ -43,14 +50,14 @@ public class MRILinearTransform : CoordinateTransform
     {
         Vector3 needlesCoord = Vector3.Scale(ccfCoord - bregma, invivoConversionAPMLDV);
         // Apply rotation
-        return RotatePointAroundPivot(needlesCoord, Vector3.zero, invivoConversionPhiThetaBeta);
+        return needlesCoord;
     }
 
-    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
-    {
-        Vector3 dir = point - pivot; // get point direction relative to pivot
-        dir = Quaternion.Euler(angles) * dir; // rotate it
-        point = dir + pivot; // calculate rotated point
-        return point; // return it
-    }
+    //public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    //{
+    //    Vector3 dir = point - pivot; // get point direction relative to pivot
+    //    dir = Quaternion.Euler(angles) * dir; // rotate it
+    //    point = dir + pivot; // calculate rotated point
+    //    return point; // return it
+    //}
 }
--- Scripts/CoordinateSystems/NeedlesTransform.cs
+++ Scripts/CoordinateSystems/NeedlesTransform.cs
@@ -47,11 +47,11 @@ public class NeedlesTransform : CoordinateTransform
         return needlesCoord;
     }
 
-    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
-    {
-        Vector3 dir = point - pivot; // get point direction relative to pivot
-        dir = Quaternion.Euler(angles) * dir; // rotate it
-        point = dir + pivot; // calculate rotated point
-        return point; // return it
-    }
+    //public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    //{
+    //    Vector3 dir = point - pivot; // get point direction relative to pivot
+    //    dir = Quaternion.Euler(angles) * dir; // rotate it
+    //    point = dir + pivot; // calculate rotated point
+    //    return point; // return it
+    //}
 }
